eb9d917dc39e8eb8157d1204a50b314275fa9d30
chesseswar <chesseswar@gmail.com>
2018-02-20 22:09:59 -0600

    output of compressed and decompressed files finished

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
             files.add(in.nextLine());
         }
 
-
         for (String s : files) {
             in = new Scanner(new File(s));
 
@@ -52,20 +51,34 @@ public class Main {
             //fill "codes" hashmap with codes for every character that appears at least once
             getCodes(frequencies.peek(), "");
 
-            //output
-            //System.out.println(codes);
-            for (char c : codes.keySet()){
-                //System.out.println(c + "  " + codes.get(c) + "  " + freqs[(int)c] + " occurrences");
-                endSize += codes.get(c).length();
-            }
-            System.out.println(s);
-            System.out.println("start size (bits): " + startSize + "\nend size (bits): " + endSize);
-            System.out.println();
-
+            //output compressed file
             PrintWriter writer = new PrintWriter(new FileWriter(s.substring(0, s.length()-4) + "COMPRESSED.txt"));
             for (char c : input){
                 writer.write(codes.get(c));
+                endSize += codes.get(c).length();
             }
+
+            //output stats (sizes of before/after files)
+            writer = new PrintWriter(new FileWriter("Compression Stats.txt"));
+            writer.write(s);
+            writer.write("start size (bits): " + startSize + "\nend size (bits): " + endSize);
+            writer.write("\n");
+
+            writer.close();
+
+            //output decompressed file
+            writer = new PrintWriter(new FileWriter(s.substring(0, s.length()-4) + "DECOMPRESSED.txt"));
+            in = new Scanner(new File(s.substring(0, s.length()-4) + "COMPRESSED.txt"));
+            char[] compressed = in.nextLine().toCharArray();
+            Node current = frequencies.peek();
+            for (int i = 0; i < compressed.length; i++){
+                current = (compressed[i] == '0' ? current.left : current.right);
+                if (current.left == null && current.right == null) {
+                    writer.write(current.value);
+                    current = frequencies.peek();
+                }
+            }
+
             writer.close();
         }
     }
